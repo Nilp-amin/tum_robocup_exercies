912c6b76765483c1dc528bf94a89cea411cd5168
nilp amin <s4581164@student.uq.edu.au>
2023-10-21 23:10:38 +0200

    exercise 2 of tutorial 1 completed

---
+++ Tutorial1_template/rover/src/applications/rover_node.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+
+#include <rover/rover.h>
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "rover_node");
+
+  rover::Rover rover;
+
+  ros::NodeHandle nh;
+  if (!rover.initialize(nh))
+  {
+    ros::shutdown();
+  }
+
+  ros::Rate rate(60);
+  ros::Time time;
+  ros::Time prev_time = ros::Time::now();
+  ros::Duration duration;
+
+  while(ros::ok())
+  {
+    time = ros::Time::now();
+    duration = time - prev_time;
+    prev_time = time;
+
+    rover.update(time, duration);
+
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
+};
