cb8cb081e73523365746edb06c9309e66de0b5d4
Nilp Amin <49432005+Nilp-amin@users.noreply.github.com>
2023-11-21 18:24:22 +0100

    Finished assignment without readme file.

--- object_labeling/src/object_labeling.cpp
+++ object_labeling/src/object_labeling.cpp
@@ -45,6 +45,7 @@ bool ObjectLabeling::initalize(ros::NodeHandle& nh)
   dict_["bottle"] = 1;
   dict_["vase"] = 2;
   dict_["toothbrush"] = 3;
+  dict_["cup"] = 4;
   // ... bananna, cup, apple, ...
 
   return true;
